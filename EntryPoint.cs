6edc6619df1a6d90b387faf00d95615d231502d3
Flowaria <wnsghgj1234@naver.com>
2023-04-01 16:05:43 +0900

    Add project files.

---
+++ EntryPoint.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+
+namespace NoGhostEnemy
+{
+    [BepInPlugin("NoGhostEnemy", "NoGhostEnemy", "1.0.0")]
+    public class EntryPoint : BasePlugin
+    {
+        // The method that gets called when BepInEx tries to load our plugin
+        public override void Load()
+        {
+            // Creates a new harmony instance to allow for patching into methods
+            m_Harmony = new Harmony("NoGhostEnemy");
+            // Apply all patches in the current assembly
+            m_Harmony.PatchAll();
+
+            // Log to the console that we have applied all patches
+            //NoGhostEnemyLogger.Verbose($"Applied all patches!");
+        }
+
+        private Harmony m_Harmony;
+    }
+}
---
+++ Patch_EarlyDeath.cs
@@ -0,0 +1,44 @@
+﻿using Enemies;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NoGhostEnemy
+{
+    [HarmonyPatch(typeof(ES_Hitreact), nameof(ES_Hitreact.ActivateState))]
+    internal static class Patch_EarlyDeath
+    {
+        internal static void Prefix(ES_Hitreact __instance, ES_HitreactType hitreactType)
+        {
+            if (hitreactType == ES_HitreactType.ToDeath)
+            {
+                var agent = __instance.m_enemyAgent;
+                var updateMode = __instance.m_enemyAgent.UpdateMode;
+
+                if (updateMode == NodeUpdateMode.None)
+                {
+                    EnemyUpdateManager.Current.Register(agent, NodeUpdateMode.Near);
+                }
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(ES_HitreactFlyer), nameof(ES_HitreactFlyer.ActivateState))]
+    internal static class Patch_EarlyDeathFlyer
+    {
+        internal static void Prefix(ES_HitreactFlyer __instance, ES_HitreactType hitreactType)
+        {
+            if (hitreactType == ES_HitreactType.ToDeath)
+            {
+                var agent = __instance.m_enemyAgent;
+                var updateMode = __instance.m_enemyAgent.UpdateMode;
+
+                if (updateMode == NodeUpdateMode.None)
+                {
+                    EnemyUpdateManager.Current.Register(agent, NodeUpdateMode.Near);
+                }
+            }
+        }
+    }
+}
